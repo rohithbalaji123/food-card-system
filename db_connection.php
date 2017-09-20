ccea8a17cfc0f5abb92722f32bda0183eee39a56
rohithbalaji123 <rohithbalaji123@gmail.com>
2017-09-20 15:18:30 +0530

    Add function to create tables if given in an array

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,26 @@
 <?php 
     
     /**
+     * function to create the database if it doesn't already exist
+     * @param  mysqli   $conn       mysqli connection object
+     * @param  string   $dbname     name of the database to be created
+     * @return boolean              true, if database is created successfully
+     */
+    function createDatabase($conn, $dbname) {
+
+        // Create the database if it doesn't already exist
+        $query = "CREATE DATABASE IF NOT EXISTS `".$dbname."`;";
+        if(! mysqli_query($conn, $query)) {
+            throw new Exception("Error creating database " . mysqli_error($conn));
+        }
+
+        return true;
+    }
+
+
+    /**
      * function to open a connection with mysql database using credentials stored in .env file
-     * @return an object of class mysqli
+     * @return  mysqli  object of class mysqli
      */
     function open_db_conn() {
 
@@ -15,14 +33,29 @@
             $env[$temp[0]] = trim($temp[1]);
         }
 
+        // Initialize environment variables
         $dbhost = $env["DB_HOST"];
         $dbuser = $env["DB_USERNAME"];
         $dbpass = $env["DB_PASSWORD"];
         $dbname = $env["DB_NAME"];
 
-        $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+        // Establish a connect with mysql
+        $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
         if (mysqli_connect_errno()) {
             throw new Exception("Failed to connect to MySQL: " . mysqli_connect_error());
+        }
+
+        // Create the database if not already exists
+        try {
+            createDatabase($conn, $dbname);
+        }
+        catch(Exception $e) {
+            throw $e;
+        }
+
+        // Connect the database to the mysqli object
+        if(!mysqli_select_db($conn, $dbname)) {
+            throw new Exception("Database connection failure.");
         }
 
         return $conn;    
@@ -30,15 +63,15 @@
 
     /**
      * function to close mysql database connection
-     * @param  object   $conn object of class mysqli
-     * @return boolean  true if connection is closed successfully
+     * @param  mysqli   $conn   object of class mysqli
+     * @return boolean          true if connection is closed successfully
      */
     function close_db_conn($conn) {
         try {
             mysqli_close($conn);
         }
         catch(Exception $e) {
-            throw new Exception($e);
+            throw $e;
         }
 
         return true;
--- migration.php
+++ migration.php
@@ -1,12 +1,66 @@
 <?php
     
     require(__DIR__ . '/db_connection.php');
+    
+    // The list of tables to be created
+    $tables = array(
+        // table_name => columns ( coumn_name => column_type)
+        "user"  =>  array(
+                        "id"    =>  "INT PRIMARY KEY AUTO_INCREMENT",
+                        "name"  =>  "VARCHAR(20) NOT NULL",
+                    ),
+    );
 
-    try {
-        $conn = open_db_conn();
-    }
-    catch(Exception $e) {
-        die($e);
+    /**
+     * Create all the tables from the tables array
+     * @param  mysqli   $conn   mysqli connection object
+     * @return bool             true, if table creation is successful
+     */
+    function createTables($conn) {
+        global $tables;
+
+        foreach($tables as $table => $columns) {
+            $query = "CREATE TABLE `".$table."`( ";
+            foreach($columns as $column => $desc) {
+                $query .= $column . " " . $desc . ",";
+            }
+
+            // trim the last comma in $query
+            $query = rtrim($query, ",");
+            $query .= ");";
+
+            if(!mysqli_query($conn, $query)) {
+                throw new Exception("Table creation unsuccessful : " . mysqli_error($conn));
+            }
+        }
+
+        return true;
     }
 
-    echo "Connection successful...";
+    /**
+     * Run migrations to open connection and create databases and tables
+     * @return bool     true, if the connection is established and tables are created
+     */
+    function migrate() {
+        
+        //Try opening a connection to database
+        try {
+            $conn = open_db_conn();
+        }
+        catch(Exception $e) {
+            die($e);
+        }
+
+        //Creating tables
+        try {
+            createTables($conn);
+        }
+        catch(Exception $e) {
+            die($e);
+        }
+
+        close_db_conn($conn);
+        echo "Table creation successful.";
+    }
+
+    migrate();
